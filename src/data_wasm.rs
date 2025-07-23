533eecdbabc65e8b694bd3b8bb38ccee9ad6e0bc
Jon Shimwell <mail@jshimwell.com>
2025-07-23 18:04:26 +0200

    added serde was bindgen

--- src/data_wasm.rs
+++ src/data_wasm.rs
@@ -1,8 +1,9 @@
 use wasm_bindgen::prelude::*;
+use serde_wasm_bindgen::to_value;
 use crate::data::NATURAL_ABUNDANCE;
 
 #[wasm_bindgen]
 pub fn natural_abundance() -> JsValue {
     let map: std::collections::HashMap<String, f64> = NATURAL_ABUNDANCE.iter().map(|(k, v)| ((*k).to_string(), *v)).collect();
-    JsValue::from_serde(&map).unwrap()
+    to_value(&map).unwrap()
 }
