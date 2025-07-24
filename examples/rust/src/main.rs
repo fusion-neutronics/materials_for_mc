df6d2968038255058d73d3ec3b321330e00190c5
Jon Shimwell <mail@jshimwell.com>
2025-07-24 14:15:28 +0200

    updated api usage

--- examples/rust/src/main.rs
+++ examples/rust/src/main.rs
@@ -10,10 +10,10 @@ fn main() {
     mat.add_nuclide("Li6", 0.05).unwrap();
     mat.set_density("g/cm3", 19.1).unwrap();
     mat.volume(Some(100.0)).unwrap();
-    // mat.calculate_microscopic_xs_neutron(None, Some(&vec!["2".to_string()]));
-    // mat.calculate_macroscopic_xs_neutron(None, Some(&vec!["2".to_string()]));
-    // mat.calculate_microscopic_xs_neutron(None, None);
-    mat.calculate_macroscopic_xs_neutron(None, None);
+    // mat.calculate_microscopic_xs_neutron(Some(&vec!["2".to_string()]));
+    // mat.calculate_macroscopic_xs_neutron(Some(&vec!["2".to_string()]));
+    // mat.calculate_microscopic_xs_neutron(None);
+    mat.calculate_macroscopic_xs_neutron(None);
 
     println!("{:?}", mat);
 
@@ -22,10 +22,10 @@ fn main() {
     lithium_mat.add_element("Li", 1.0).unwrap();
     lithium_mat.set_density("g/cm3", 0.534).unwrap(); // Lithium density
     lithium_mat.volume(Some(50.0)).unwrap();
-    // lithium_mat.calculate_microscopic_xs_neutron(None, Some(&vec!["2".to_string()]));
-    // lithium_mat.calculate_macroscopic_xs_neutron(None, Some(&vec!["2".to_string()]));
-    // lithium_mat.calculate_microscopic_xs_neutron(None, None);
-    lithium_mat.calculate_macroscopic_xs_neutron(None, None);
+    // lithium_mat.calculate_microscopic_xs_neutron(Some(&vec!["2".to_string()]));
+    // lithium_mat.calculate_macroscopic_xs_neutron(Some(&vec!["2".to_string()]));
+    // lithium_mat.calculate_microscopic_xs_neutron(None);
+    lithium_mat.calculate_macroscopic_xs_neutron(None);
 
     // println!("Lithium material: {:?}", lithium_mat);
     // println!("Lithium material: {:?}", lithium_mat);
